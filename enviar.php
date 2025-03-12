4d2afd031e77814ba3f55508df96db43ae030226
estelamartinez <estelaerasmus@gmail.com>
2025-03-12 11:55:04 +0100

    cambios

---
+++ enviar.php
@@ -0,0 +1,56 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
+if($_POST){
+    $asuntos=$_POST['asunto'];
+    $descripcion=$_POST['descripcion'];
+}
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'libs/vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'paginaswebifcd@gmail.com';                     //SMTP username
+    $mail->Password   = 'ekheppkkfjzycjgp';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('remitente@gmail.com', 'Estela');
+    $mail->addAddress('paginaswebifcd@gmail.com', 'Estela'); 
+
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $asunto;
+    $mail->Body    = "
+        <p><strong>Nombre:</strong> $nombre</p>
+        <p><strong>Teléfono:</strong> $telefono</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Aunto:</strong> $aunto</p>
+        <p><strong>Descripción:</strong></p>
+        <p>$descripcion</p>
+    ";
+
+    $mail->send();
+    header("Location:index.php");
+    echo 'correo ha sido enviado';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
