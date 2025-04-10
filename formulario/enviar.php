68bd31ece4ad79bf3f27e1a3b88f0f0a78050df3
ruben2125 <undergroundvibes25@gmail.com>
2025-04-10 14:14:33 +0200

    cambiosbb

--- formulario/enviar.php
+++ formulario/enviar.php
@@ -24,14 +24,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rubenredecillas21@gmail.com';                     //SMTP username
-    $mail->Password   = 'pgxwsmudmujcsxeh';                               //SMTP password
+    $mail->Username   = 'castillolaguardiadejaen@gmail.com';                     //SMTP username
+    $mail->Password   = 'kmcbojiixgkpcokn';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('remitente@gmail.com');
-    $mail->addAddress('rubenredecillas21@gmail.com', 'Ruben'); 
+    $mail->setFrom('remitente@gmail.com', 'Solicitante');
+    $mail->addAddress('castillolaguardiadejaen@gmail.com', 'Monumentos'); 
 
 
 
